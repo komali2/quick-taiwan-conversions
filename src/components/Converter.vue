30b02953c9b237a609c770289e638a48d6534b97
Morgan Elliot <m.33.elliot@gmail.com>
2022-01-10 15:09:04 +0800

    Changed component into Converter
    Changed it to pass a function as a prop

---
+++ src/components/Converter.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="converter">
+    <p>{{ label }}</p>
+    <input v-model.number="convert" type="number" placeholder="Enter USD" />
+    <p>{{ outputLabel }} {{ convertedOutput }}</p>
+  </div>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Converter",
+  props: {
+    label: String,
+    outputLabel: String,
+    output: Function,
+  },
+  data() {
+    return {
+      convert: ""
+    };
+  },
+ 
+  computed: {
+    convertedOutput() {
+      //return this.convert * 30;
+      return this.output(this.convert);
+    },
+  },
+};
+</script>
