4b30331adb5c6db47277f6df48c399878f28af8a
Morgan Elliot <m.33.elliot@gmail.com>
2022-01-12 01:07:02 +0800

    Added CSS to begin designing a pleasing layout Still needs a lot of work but much of the framework is in place

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,15 +1,76 @@
 <template>
-  <div class="hello">
+  <div id="heading">
     <h1>{{ msg }}</h1>
-    <p>New in Taiwan?<br />Here are some converters to help you out!<br /></p>
+    <h2>New in Taiwan?<br />Here are some converters to help you out!<br /></h2>
   </div>
-  <div>
-    <Converter label="USD - NTD" outputLabel="NTD Equivalent" :output="usdConvert" enterVal="Enter USD" />
-    <Converter label="NTD - USD" outputLabel="USD Equivalent" :output="ntdConvert" enterVal="Enter NTD" />
-    <Converter label="MPH - KPH" outputLabel="KPH Equivalent" :output="mphConvert" enterVal="Enter MPH" />
-    <Converter label="KPH - MPH" outputLabel="MPH Equivalent" :output="kphConvert" enterVal="Enter KPH" />
-    <Converter label="F ° - C °" outputLabel="C ° Equivalent" :output="fConvert" enterVal="Enter Degrees F" />
-    <Converter label="C ° - F °" outputLabel="F ° Equivalent" :output="cConvert" enterVal="Enter Degrees C" />
+  <div id="content">
+    <div id="converters">
+      <div>
+        <Converter
+          label="USD - NTD"
+          outputLabel="NTD Equivalent"
+          :output="usdConvert"
+          enterVal="Enter USD"
+          id="usd"
+        />
+      </div>
+      <div>
+        <Converter
+          label="NTD - USD"
+          outputLabel="USD Equivalent"
+          :output="ntdConvert"
+          enterVal="Enter NTD"
+        />
+      </div>
+      <div>
+        <Converter
+          label="MPH - KPH"
+          outputLabel="KPH Equivalent"
+          :output="mphConvert"
+          enterVal="Enter MPH"
+        />
+      </div>
+      <div>
+        <Converter
+          label="KPH - MPH"
+          outputLabel="MPH Equivalent"
+          :output="kphConvert"
+          enterVal="Enter KPH"
+        />
+      </div>
+      <div>
+        <Converter
+          label="F  ° - C  °"
+          outputLabel="C  ° Equivalent"
+          :output="fConvert"
+          enterVal="Enter Degrees F"
+        />
+      </div>
+      <div>
+        <Converter
+          label="C  ° - F  °"
+          outputLabel="F  ° Equivalent"
+          :output="cConvert"
+          enterVal="Enter Degrees C"
+        />
+      </div>
+      <div>
+        <Converter
+          label="Inch - Millimeter"
+          outputLabel="Millimeter Equivalent"
+          :output="inConvert"
+          enterVal="Enter Inches"
+        />
+      </div>
+      <div>
+        <Converter
+          label="Millimeter - Inch"
+          outputLabel="Inch Equivalent"
+          :output="mmConvert"
+          enterVal="Enter Millimeters"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -36,16 +97,21 @@ export default {
         return Number(Math.round(convert * 0.62 * 10) / 10);
       },
       fConvert(convert) {
-        if(Number(convert) === 0) return Number(0);
-        else return Number(Math.round((convert - 32) * 5 / 9));
+        if (Number(convert) === 0) return Number(0);
+        else return Number(Math.round((((convert - 32) * 5) / 9) * 10) / 10);
       },
       cConvert(convert) {
-        if(Number(convert) === 0) return Number(0);
-        else return Number(Math.round((convert *9 / 5) + 32));
+        if (Number(convert) === 0) return Number(0);
+        else return Number(Math.round(((convert * 9) / 5 + 32) * 10) / 10);
+      },
+      inConvert(convert) {
+        return Number(Math.round(convert * 25.4 * 10) / 10);
+      },
+      mmConvert(convert) {
+        return Number(Math.round(convert * 0.03937007874015748 * 1000) / 1000);
       },
     };
   },
-
   components: {
     Converter,
   },
@@ -54,18 +120,26 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+h1 {
+  padding-top: 50px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+h2 {
+  padding-bottom: 30px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+#content {
+  padding-bottom: 100px;
 }
-a {
-  color: #42b983;
+#converters {
+  width: 500px;
+  margin: auto;
+  display: grid;
+  grid-template-columns: 200px 200px;
+  grid-gap: 1em 1em;
+  justify-content: center;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  background-color: rgba(84, 147, 240, 0.75);
+  border: 3px solid #383;
+  border-radius: 20px;
 }
 </style>
--- src/components/Converter.vue
+++ src/components/Converter.vue
@@ -1,32 +1,53 @@
 <template>
-  <div id="converter">
-    <p>{{ label }}</p>
-    <input v-model.number="convert" type="number" :placeholder="enterVal" />
-    <p>{{ outputLabel }} {{ convertedOutput }}</p>
-  </div>
-  <div>
-  </div>
+ <div id="converter">
+   <p>{{ label }}</p>
+   <input v-model.number="convert" type="number" :placeholder="enterVal" />
+   <p>{{ outputLabel }}</p><p id="result">{{ convertedOutput }}</p>
+ </div>
+ <div>
+ </div>
 </template>
-
+ 
 <script>
 export default {
-  name: "Converter",
-  props: {
-    label: String,
-    outputLabel: String,
-    enterVal: String,
-    output: Function,
-  },
-  data() {
-    return {
-      convert: ""
-    };
-  },
- 
-  computed: {
-    convertedOutput() {
-      return this.output(this.convert);
-    },
-  },
+ name: "Converter",
+ props: {
+   label: String,
+   outputLabel: String,
+   enterVal: String,
+   output: Function,
+ },
+ data() {
+   return {
+     convert: ""
+   };
+ },
+ computed: {
+   convertedOutput() {
+     return this.output(this.convert);
+   },
+ },
 };
 </script>
+ 
+<style scoped>
+#converter {
+  padding: 10px;
+}
+input {
+  display: flex;
+  width: 80%;
+  margin: auto;
+  padding: 10px 14px;
+  border: 3px solid #383;
+  border-radius: 7px;
+  font-size: 14pt;
+}
+p {
+  font-size: 14pt;
+  font-weight: bold;
+}
+#result {
+  font-size: 18pt;
+}
+</style>
